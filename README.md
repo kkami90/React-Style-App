eebf8c8b839deddd4956b6df539326554b980283
kkami <kkami9090@google.com>
2020-09-28 11:27:20 +0900

    에어비엔비 디자인하기 환경설정 완료

--- README.md
+++ README.md
@@ -26,3 +26,17 @@
 ### 라이브러리
 
 - npm install --save styled-components
+
+### Git 관리
+
+- git init (깃 박스 3개를 만든다)
+- git add . (두반째 박스로 옴기는 작업)
+- git commit -m "1. v1" (3번째 박스로 옴기는 작업)
+
+- github에 저장소 만들기(ex: www. hello.com란 곳에 만들시)
+
+- git remote add origin www.hello.com (원격지 연결)
+- git push origin master(업로드)
+
+- git pull origin master(다운로드)
+- npm install (의존성 라이브러리 다운로드. 해당 작업을 해야 node모듈들이 다운됨)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
+  const Mybox = styled.div`
+    display: grid;
+    grid-template-columns: auto auto;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Mybox>1qqqq</Mybox>
+      <Mybox>2qqqq</Mybox>
     </div>
   );
 }
